574e979bd3c56614966c729ea52ebbf00f51983f
Vo Le Vinh Nghia <vn29082000@gmail.com>
2023-06-19 14:50:22 +0700

    Update

--- src/routes/writefile.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const writeFileControllers = require('../controllers/writefile');
-
-
-router.post('/file',writeFileControllers.writeFileJSON);
-
-module.exports = router;
